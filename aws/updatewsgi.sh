31d78f07703d1dd0b8a991fb1b4684cf00825860
Dzmitry Zhaleznichenka <Dzmitry.Zhaleznichenka@gmail.com>
2012-10-27 00:11:10 +0200

    Got rid of bloody Beanstalk

--- aws/updatewsgi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /etc/httpd/conf.d
-if !(grep --quiet 'Alias /static /opt/python/current/app/static/' wsgi.conf)
-    then
-        sudo sed -i 's:Alias /static /opt/python/current/app/:Alias /static /opt/python/current/app/static/:' wsgi.conf
-fi
-echo Script done
